90c9f7c3f7a96abfb43c48439434667ec76740a2
pauparessola <pauparessola@gmail.com>
2026-02-28 07:19:10 +0100

    Robustesa en els inputs Imatges en el resultat Comentaris

--- frontend/index.js
+++ frontend/index.js
@@ -40,29 +40,50 @@ function mostrarDiners(diners){
 }
 
 function mostrarImatge(ruta) {
+    const img = document.getElementById("imgResultat");
     img.src = ruta;
     img.style.display = "block";
 }
 
-// ----------------------------------------------------
+// ---------------------------------------------------------------
 
 console.log('Happy horse gambling!!!!!!!!!! ✨');
 
 const caballoCount = [0,0,0,0,0];
 
-
+// Inicialitzem els diners
 let dinersPropis = 100;
 mostrarDiners(dinersPropis);
 
+// Declarem el Button i els RadioButtons
 const botonApostar = document.getElementById('botonApostar');
 const radios = document.querySelectorAll('input[name="opcio"]');
+const texto = document.getElementById('textBox');
 
-// Afegir listeners a cada opció del radio
+// Afegir listeners a cada opció dels RadioButtons
 radios.forEach(radio => {
     radio.addEventListener("change", () => {
-        botonApostar.disabled = false;
+        let apostaActual = parseFloat(radio.value);
+
+        if (apostaActual <= dinersPropis && apostaActual > 0) {
+            // Un cop s'ha fet una selecció, si hi ha un número vàlid es pot activar
+            botonApostar.disabled = false;
+        }
     });
 });
+
+
+// EventListener per la textbox per fer robust si el número introduit es vàlid
+texto.addEventListener('change', () => {
+    let apostaActual = parseFloat(radio.value);
+
+    if (apostaActual <= dinersPropis && apostaActual > 0 && document.querySelector('input[name="opcio"]:checked') != null){
+        // Si s'introdueix un número vàlid i s'ha fet una selecció es pot activar
+        botonApostar.disabled = false;
+    } else {
+        botonApostar.disabled = true;
+    }
+})
 
 // Apostar
 botonApostar.addEventListener("click", () => {
@@ -70,17 +91,12 @@ botonApostar.addEventListener("click", () => {
     if (!checked) return;
     const cavallTriat = Number(checked.value); // Obtenir caval triat
 
-    let status = document.getElementById("statusText"); // Pren el <p> de status]
-    let history = document.getElementById("statusHistory");
-    const img = document.getElementById("imgResultat");
-
-    let aposta = parseInt(document.getElementById("textBox").value);
+    let status = document.getElementById("statusText"); // Pren el <p> de status
+    let aposta = parseInt(document.getElementById("textBox").value); // Obtenim el valor de l'aposta
 
     console.log(cavallTriat);
-
-    //history.innerHTML += `\n Ha guanyat `
-
     console.log(`${dinersPropis} dinersPropis`);
+
     if (resultatsJoc(cavallTriat)) {
         dinersPropis = dinersPropis + aposta;
         status.textContent = `Siii has guanyat ${aposta} aposta mes!!!`;
@@ -90,6 +106,7 @@ botonApostar.addEventListener("click", () => {
         status.textContent = `Nooo has perdut ${aposta} aposta mes!!!`;
         mostrarImatge('/frontend/images/disappointment.png');
     }
+
     console.log(`${dinersPropis} dinersPropis`);
     mostrarDiners(dinersPropis);
 });
